cc09a50a086730a6c31be0ccf480c04c453ddff4
Hammad Malik <tomatohammado@gmail.com>
2019-06-26 16:03:16 -0400

    Complete addKingdom.js
    - now I want to play around with the sync/async fs methods

--- json/process/addKingdom.js
+++ json/process/addKingdom.js
@@ -2,4 +2,26 @@ const fs = require("fs");
 
 const { kingdoms } = require("./util");
 
-console.log(kingdoms);
+kingdoms.forEach(kingdom => {
+  const kingdomFilename = kingdom.toLocaleLowerCase().replace(/ /g, "-");
+  let kingdomData = fs.readFileSync(
+    `./json/raw/${kingdomFilename}.json`,
+    "utf8"
+  );
+  console.log(`Read Data: ${kingdom}`);
+
+  kingdomData = JSON.parse(kingdomData);
+
+  kingdomData.forEach(data => {
+    data.kingdom = kingdom;
+  });
+
+  fs.writeFile(
+    `./json/withKingdom/${kingdomFilename}.json`,
+    JSON.stringify(kingdomData),
+    err => {
+      if (err) throw err;
+      console.log(`Finish processing: ${kingdom}`);
+    }
+  );
+});
